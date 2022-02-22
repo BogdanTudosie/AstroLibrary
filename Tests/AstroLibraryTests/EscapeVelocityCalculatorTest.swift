0c79619d121ff1cb8fd3be43618a46335d362783
Bogdan Tudosie <bogdan.tudosie@f-secure.com>
2022-02-22 14:27:02 +0200

    Initial Commit

---
+++ Tests/AstroLibraryTests/EscapeVelocityCalculatorTest.swift
@@ -0,0 +1,42 @@
+//
+//  EscapeVelocityCalculator.swift
+//  
+//
+//  Created by Tudosie, Bogdan on 18.2.2022.
+//
+
+import XCTest
+@testable import AstroLibrary
+
+class EscapeVelocityCalculatorTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let escapeVelCal = EscapeVelocityCalculator(radius: 6371, gravConstant: 6.671 * pow(10, -11), mass: 5.972 * pow(10, 24))
+        let value = escapeVelCal.calculateEscapeVelocity()
+        print(value)
+        XCTAssert(value > 0)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
